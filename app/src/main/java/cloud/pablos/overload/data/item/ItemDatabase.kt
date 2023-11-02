1400633e4708b70083812365d5ace84b7725b40e
pabloscloud <93644977+pabloscloud@users.noreply.github.com>
2023-11-02 07:15:52 +0100

    Create pages

--- app/src/main/java/cloud/pablos/overload/data/item/ItemDatabase.kt
+++
@@ -1,9 +0,0 @@
-package cloud.pablos.overload.data.item
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Item::class], version = 1)
-abstract class ItemDatabase : RoomDatabase() {
-    abstract fun itemDao(): ItemDao
-}
--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.gradle.versions)
-    alias(libs.plugins.version.catalog.update)
-}
-
-apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
